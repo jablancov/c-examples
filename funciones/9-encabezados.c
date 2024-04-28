2dc529880237f6d77e534bb2484aa405f2f5b564
Jorge Blanco <jorge.blancovasquez@ucr.ac.cr>
2024-04-28 15:34:55 -0600

    Correccion de un typo que no me permitia compilar, no es importante para el laboratorio

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -50,7 +50,7 @@ int funcion4(int x) {
 
 // funcion5(): Imprime un mensaje. Retorna el valor que recibió como argumento
 // incrementado en 5
-int funcion3(int x) {
+int funcion5(int x) {
     printf("En funcion5()...\n");
     int y = x + 5;
     return y;
